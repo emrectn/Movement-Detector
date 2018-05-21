562518b7c80b76c7f38de7a36478304278b7bd00
Emre-Win8 <emrec.tin@gmail.com>
2018-05-21 18:47:14 +0300

    add movement state and new activity

--- app/src/main/java/com/example/user/sensorsample/DurumGecmisiActivity.java
+++ app/src/main/java/com/example/user/sensorsample/DurumGecmisiActivity.java
@@ -10,11 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class DurumGecmisiActivity extends AppCompatActivity {
 
-    private Button btn_back;
+    private Button btn_back, btn_next;
     private TextView txt_veri, txt_veri1, txt_veri2;
     private  int id, walking_time, stop_time, running_time;
     private ArrayList<String> state_list = new ArrayList<String>();
@@ -22,18 +24,54 @@ public class DurumGecmisiActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private String TAG = getClass().getSimpleName();
 
+    private final static String  jString = "{"
+            + "    \"geodata\": ["
+            + "        {"
+            + "                \"id\": \"1\","
+            + "                \"name\": \"Julie Sherman\","
+            + "                \"gender\" : \"female\","
+            + "                \"latitude\" : \"37.33774833333334\","
+            + "                \"longitude\" : \"-121.88670166666667\""
+            + "                }"
+            + "        },"
+            + "        {"
+            + "                \"id\": \"2\","
+            + "                \"name\": \"Johnny Depp\","
+            + "                \"gender\" : \"male\","
+            + "                \"latitude\" : \"37.336453\","
+            + "                \"longitude\" : \"-121.884985\""
+            + "                }"
+            + "        }"
+            + "    ]"
+            + "}";
+    private static JSONObject jObject = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_durum_gecmisi);
+
+        sharedPref = this.getSharedPreferences("sensor", Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
 
         txt_veri = findViewById(R.id.txt_veri);
         txt_veri1 = findViewById(R.id.txt_veri1);
         txt_veri2 = findViewById(R.id.txt_veri2);
 
         btn_back = findViewById(R.id.btn_back);
-        sharedPref = this.getSharedPreferences("sensor", Context.MODE_PRIVATE);
-        editor = sharedPref.edit();
+        btn_next = findViewById(R.id.btn_next);
+
+        final String JSON_STRING="{\"employee\":{\"name\":\"Sachin\",\"salary\":56000}}";
+        try{
+            JSONObject emp=(new JSONObject(JSON_STRING)).getJSONObject("employee");
+            String empname=emp.getString("name");
+            int empsalary=emp.getInt("salary");
+
+            String str="Employee Name:"+empname+"\n"+"Employee Salary:"+empsalary;
+            System.out.println(str);
+
+        }catch (Exception e) {e.printStackTrace();}
+        //Do when JSON has problem.
 
 
         Intent i = getIntent();
@@ -66,6 +104,15 @@ public class DurumGecmisiActivity extends AppCompatActivity {
             }
         });
 
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent movement_intent = new Intent(DurumGecmisiActivity.this, StateListActivity.class);
+                movement_intent.putExtra("id", id);
+                startActivity(movement_intent);
+            }
+        });
+
 
     }
 
@@ -78,7 +125,9 @@ public class DurumGecmisiActivity extends AppCompatActivity {
 
     public void getStateList(){
         for (int i=id; i>=0; i--){
-            state_list.add(getSharedPreference(""+ i));
+            String s = getSharedPreference(""+ i);
+
+            state_list.add(s);
         }
 
 //        for (String a : state_list) {
